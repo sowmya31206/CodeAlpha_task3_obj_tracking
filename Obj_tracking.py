9a4b8050e80337acc81a9a96d26d3fe7ea20c6bb
sowmya31206 <sowmyashanmugavel31206@gmail.com>
2026-07-15 18:10:34 +0530

    Update Obj_tracking.py

--- Obj_tracking.py
+++ Obj_tracking.py
@@ -1,24 +1,30 @@
 !pip install ultralytics opencv-python
+
 import os
 
 for root, dirs, files in os.walk("/content/runs"):
     print(root)
     print(files)
+
 import cv2
 import os
 
 from ultralytics import YOLO
 from google.colab import files
 from IPython.display import Video, display
+
 model = YOLO("yolov8n.pt")
 
 print("YOLO model loaded successfully!")
+
 !rm -rf /content/runs
+
 uploaded = files.upload()
 
 video_path = list(uploaded.keys())[0]
 
 print("Uploaded Video:", video_path)
+
 results = model.track(
     source=video_path,
     save=True,
@@ -27,7 +33,10 @@ results = model.track(
 )
 
 print("Detection completed!")
-!ls -lh /content/runs/detect/track
-!ffmpeg -y -i "/content/runs/detect/track/WhatsApp Video 2026-07-12 at 11.30.59 AM (1) (2).avi" output.mp4
-from IPython.display import Video, display
-display(Video("output.mp4", embed=True))
+
+!ffprobe /content/runs/detect/track/*.avi
+
+!ffmpeg -i /content/runs/detect/track/*.avi -vcodec libx264 output.mp4
+
+from IPython.display import Video
+Video("output.mp4", embed=True)
